50d8089cfcfef8106fadc1ce1fcf19ede66c2780
Steven <ksteve998@gmail.com>
2023-06-15 11:32:30 -0500

    regroup

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -1,23 +1,35 @@
-const Sequelize = require("sequelize");
-const db = require("../db.js");
+const Sequelize = require('sequelize')
+const db = require('../db')
 
-const Product = db.define("Product", {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.TEXT,
-    allowNull: true,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    defaultValue: 'https://www.creativeuncut.com/gallery-22/art/bdff-background-art.jpg',
-  }
-});
+const Art = db.define('Product', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    artisit: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    year: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.NUMBER,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://www.creativeuncut.com/gallery-22/art/bdff-background-art.jpg',
+      },
+    sellerId: {
+        type: Sequelize.VIRTUAL,
+        allowNull: false
+    }
+})
 
-module.exports = Product
+module.exports = Art
