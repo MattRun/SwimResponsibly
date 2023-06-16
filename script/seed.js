45867f543ff85b29475d8ee82ec968f1bb4d3a9c
kgr718 <Keithg_russell@yahoo.com>
2023-06-15 20:08:39 -0400

    Dummy Data

--- script/seed.js
+++ script/seed.js
@@ -19,30 +19,42 @@ async function seed() {
   // Creating Products
   const products = await Promise.all([
     Product.create({
-      name: 'Product 1',
-      price: 10.99,
-      description: 'This is the first product.',
+      title: 'Starry Night',
+      artist: "Vincent Vangoh",
+      year: 1889,
+      description: 'If you love stars and love nights, then boy do i have a paiting for you!',
+      price: 1000000,
+      sellerId: 1,
+      imageUrl: "https://sanctuarymentalhealth.org/wp-content/uploads/2021/03/The-Starry-Night-1200x630-1-979x514.jpg"
     }),
     Product.create({
-      name: 'Product 2',
-      price: 19.99,
-      description: 'This is the second product.',
+      title: 'Dinosaur',
+      artist: "Jean Micheal Basquiat",
+      year: 1980,
+      description: 'If you love dinosaurs with a crown, then boy do i have a paiting for you!',
+      price: 200000,
+      sellerId: 2,
+      imageUrl:"https://images.ctfassets.net/9y4fi2gkk4ov/vLXFHdbfSLv4Pb6Oi3i6d/40eec6cb9d87d892dcbc2ed873632e37/Jean-Michel_Basquiat_Dino_Doormat_DM23.jpg"
     }),
     Product.create({
-      name: 'Product 3',
-      price: 7.99,
-      description: 'This is the third product.',
+      title: 'Spirit Molecule',
+      artist: "Alex Gray",
+      year: 2004,
+      description: 'If you love trippy stuff, then boy do i have a paiting for you!',
+      price: 1000000,
+      sellerId: 3,
+      imageUrl:"https://www.alexgrey.com/art-images/Godself-2012-Alex-Grey-watermarked.jpeg"
+      
     }),
     Product.create({
-      name: 'Product 4',
-      price: 14.99,
-      description: 'This is the fourth product.',
-    }),
-    Product.create({
-      name: 'Product 5',
-      price: 24.99,
-      description: 'This is the fifth product.',
-    }),
+      title: 'I Donut Care',
+      artist: "Claudette Medrano",
+      year: 2020,
+      description: 'Be nice, this one is my sisters art',
+      price: 4000000000,
+      sellerId: 3,
+      imageUrl:"https://scontent-lga3-1.xx.fbcdn.net/v/t1.6435-9/195161645_766621590697726_9154232031115017028_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a26aad&_nc_ohc=Itd6d3vlL-8AX-tASiP&_nc_ht=scontent-lga3-1.xx&oh=00_AfCV-Azh7Yo12oSkbQ-EOHSc2hWapsfFHiTuaArHJabNUw&oe=64B282C0"
+      }),
   ]);
 
 
