05b22f065f4020351f7cc2f8ed30168534b9afd1
Steven <ksteve998@gmail.com>
2023-06-20 10:13:19 -0500

    merging

--- server/db/db.js
+++ server/db/db.js
@@ -24,8 +24,12 @@ if(process.env.DATABASE_URL){
 
 
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://postgres@localhost:5432/${databaseName}`, config)
-
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://postgres@localhost:5432/${databaseName}`, config)
+  const db  = new Sequelize(
+    databaseName, 
+    "postgres", 
+    "Openup", 
+    config);
 
 module.exports = db
